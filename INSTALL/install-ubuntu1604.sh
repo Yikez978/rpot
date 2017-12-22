b232977bae1d1b33e6e949e2008e9be8f3f7e192
super_a1ice <super.alice.002@gmail.com>
2017-12-23 02:28:47 +0900

    modify install script

--- INSTALL/install-ubuntu1604.sh
+++ INSTALL/install-ubuntu1604.sh
@@ -93,7 +93,18 @@ sudo ln -sf /usr/local/bin/node /usr/bin/node
 sudo apt-get purge -y nodejs npm
 sudo npm install elasticdump -g
 
+# install yara
+wget https://github.com/VirusTotal/yara/archive/v3.6.3.tar.gz -O yara.tar.gz
+tar -zxf yara.tar.gz
+cd yara-3.6.3 
+./bootstrap.sh
+./configure
+make -j 4
+sudo make install
+cd ..
+
 # install PasteHunter
+sudo pip install yara-python
 sudo git clone https://github.com/vishnubob/wait-for-it /opt/wait-for-it
 sudo git clone https://github.com/kevthehermit/PasteHunter /opt/PasteHunter
 sudo chown -R ${INSTALL_USER}:${INSTALL_USER} /opt/PasteHunter
