999903e88c5d689852d129146c0f4bdb6d760984
super_a1ice <super.alice.002@gmail.com>
2017-12-24 23:04:15 +0900

    modify restore script

--- restore-index.sh
+++ restore-index.sh
@@ -1,3 +1,5 @@
 gzip -dc backup_bro.json.gz |elasticdump --input=$ --output='http://localhost:9200/bro-restore'  
 gzip -dc backup_clamd.json.gz |elasticdump --input=$ --output='http://localhost:9200/clamd-restore'  
 gzip -dc backup_suricata.json.gz |elasticdump --input=$ --output='http://localhost:9200/suricata-restore'  
+gzip -dc backup_twitter.json.gz |elasticdump --input=$ --output='http://localhost:9200/twitter-restore'  
+gzip -dc backup_paste.json.gz |elasticdump --input=$ --output='http://localhost:9200/paste-restore'  
